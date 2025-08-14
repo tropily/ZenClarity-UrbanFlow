fe444a7434cef9169fcf3b8617a433307f784cad
Teo <viperuser@DESKTOP-2B9C7GL.localdomain>
2025-08-14 08:09:49 -0700

    add redshift env & dbt redshift lessons learn

--- N_LeTaxity/models/marts/nyc_taxi_dashboard/agg_daily_trips.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    DATE(t.pickup_at) AS pickup_date,
-    HOUR(t.pickup_at) AS pickup_hour,
-    z.zone AS pickup_zone,
-    COUNT(*) AS total_trips
-FROM {{ ref('stg_nyc_taxi__trip_data') }} AS t
-LEFT JOIN {{ ref('stg_nyc_taxi__taxi_zone_lookup') }} AS z
-    ON t.pickup_location_id = z.location_id
-GROUP BY 1, 2, 3
