bf3dfceb8b69b51852c5bfa6fa458fe9a91157e6
Nguyen Le <63549662+tropily@users.noreply.github.com>
2025-09-01 11:36:27 -0700

    chore: force remove N_LeTaxity_ax

--- N_LeTaxity_ax/dbt/models/marts/nyc_taxi_dashboard/agg_monthly_cab_type_trip_counts.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='view') }}
-
-select
-  cab_type,
-  pickup_month,
-  count(*) as trip_count
-from {{ ref('int_nyc__trip_zone') }}
-group by 1,2
--- N_LeTaxity_ax/dbt/models/staging/nyc_taxi/stg_nyc_taxi__taxi_zone_lookup.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  location_id,
-  borough,
-  zone,
-  service_zone,
-  filename_loaded
-FROM {{ source('nyc_taxi_db', 'taxi_zone_lookup') }}
