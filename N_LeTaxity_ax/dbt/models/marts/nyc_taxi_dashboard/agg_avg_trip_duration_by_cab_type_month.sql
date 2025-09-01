fc1a0e3b9885fb5c31bd546ae410a065c3b7d456
Nguyen Le <63549662+tropily@users.noreply.github.com>
2025-09-01 11:36:27 -0700

    chore: force remove N_LeTaxity_ax

--- N_LeTaxity_ax/dbt/models/marts/nyc_taxi_dashboard/agg_avg_trip_duration_by_cab_type_month.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='view') }}
-
-select
-  cab_type,
-  pickup_month,
-  avg(trip_duration_min) as avg_trip_duration_min
-from {{ ref('int_nyc__trip_zone') }}
-where trip_duration_min is not null
-group by 1,2
